6a367ef80e64797413c7ca86a2e519d05d0a5f1a
Jennifer <jennifer_brown_k@163.com>
2024-07-07 17:34:43 +0900

    Create fun_fibonacci.py

---
+++ fun_fibonacci.py
@@ -0,0 +1,10 @@
+# fun_fibonacci.py
+def fun_fibonacci(n):
+    a, b = 0, 1
+    result = []
+    while len(result) < n:
+        result.append(a)
+        a, b = b, a + b
+    return result
+
+print(fun_fibonacci(10))
