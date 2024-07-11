3b840a88076787cc25cd712654fb54620f105cb0
Jennifer <jennifer_brown_k@163.com>
2024-07-11 20:25:46 +0900

    Create func_generate_prime_numbers.py

---
+++ func_generate_prime_numbers.py
@@ -0,0 +1,12 @@
+# func_generate_prime_numbers.py
+def func_generate_prime_numbers(limit):
+    primes = []
+    for num in range(2, limit + 1):
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                break
+        else:
+            primes.append(num)
+    return primes
+
+print(func_generate_prime_numbers(30))
