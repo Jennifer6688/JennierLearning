b20020c71e33779eccc0b812ba71830215d4a957
Jennifer <jennifer_brown_k@163.com>
2024-08-06 19:58:02 +0900

    Create func_py_calculate_rectangle_perimeter.py

---
+++ func_py_calculate_rectangle_perimeter.py
@@ -0,0 +1,5 @@
+# func_py_calculate_rectangle_perimeter.py
+def func_py_calculate_rectangle_perimeter(length, width):
+    return 2 * (length + width)
+
+print(func_py_calculate_rectangle_perimeter(5, 3))
