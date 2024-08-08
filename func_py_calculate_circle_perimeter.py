dcf91a7a448ce1c3a3f36bee6e1cf82497cf3796
Jennifer <jennifer_brown_k@163.com>
2024-08-08 10:55:27 +0900

    Create func_py_calculate_circle_perimeter.py

---
+++ func_py_calculate_circle_perimeter.py
@@ -0,0 +1,7 @@
+# func_py_calculate_circle_perimeter.py
+import math
+
+def func_py_calculate_circle_perimeter(radius):
+    return 2 * math.pi * radius
+
+print(func_py_calculate_circle_perimeter(5))
