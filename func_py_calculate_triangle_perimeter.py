39559a3fc9e97b91f6a731cebb7b502a107da41a
Jennifer <jennifer_brown_k@163.com>
2024-08-09 10:25:36 +0900

    Create func_py_calculate_triangle_perimeter.py

---
+++ func_py_calculate_triangle_perimeter.py
@@ -0,0 +1,5 @@
+# func_py_calculate_triangle_perimeter.py
+def func_py_calculate_triangle_perimeter(side1, side2, side3):
+    return side1 + side2 + side3
+
+print(func_py_calculate_triangle_perimeter(3, 4, 5))
