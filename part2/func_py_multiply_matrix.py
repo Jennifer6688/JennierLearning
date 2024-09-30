e9e24e1cdd973f61852d375f14c8b607541fe862
Jennifer <jennifer_brown_k@163.com>
2024-09-30 16:11:19 +0900

    Create func_py_multiply_matrix.py

---
+++ part2/func_py_multiply_matrix.py
@@ -0,0 +1,17 @@
+# func_py_multiply_matrix.py
+def func_py_multiply_matrix(a, b):
+    rows_a = len(a)
+    cols_a = len(a[0])
+    rows_b = len(b)
+    cols_b = len(b[0])
+
+    if cols_a != rows_b:
+        return "Matrix multiplication not possible"
+
+    result = [[0 for _ in range(cols_b)] for _ in range(rows_a)]
+
+    for i in range(rows_a):
+        for j in range(cols_b):
+            for k in range(cols_a):
+                result[i][j] += a[i][k] * b[k][j]
+    return result
